882c4a87e48e8882788ef52fd27ad1a5770baf69
Saulo Costa <79532045+saulomlcosta@users.noreply.github.com>
2022-09-07 19:23:18 -0300

    cronometer in progress

---
+++ tsxcronometer/src/components/Formulario/index.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { ITarefa } from '../../types/tarefa';
+import Botao from '../Botao';
+import style from './Formulario.module.scss';
+
+class Formulario extends React.Component<{
+    setTarefas: React.Dispatch<React.SetStateAction<ITarefa[]>>
+}> {
+
+    state = {
+        tarefa: "",
+        tempo: "00:00"
+    }
+
+    adicionarTarefa(evento: React.FormEvent<HTMLFormElement>) {
+        evento.preventDefault();
+        this.props.setTarefas(tarefasAntigas => [...tarefasAntigas, { ...this.state }])
+    }
+
+    render() {
+        return (
+            <form className={style.novaTarefa} onSubmit={this.adicionarTarefa.bind(this)}>
+                <div className={style.inputContainer}>
+                    <label htmlFor="tarefa">
+                        Adicione um novo estudo
+                    </label>
+                    <input
+                        type="text"
+                        name="tarefa"
+                        id="tarefa"
+                        value={this.state.tarefa}
+                        onChange={evento => this.setState({ ...this.state, tarefa: evento.target.value })}
+                        placeholder="O que você quer estudar"
+                        required
+                    />
+                </div>
+                <div className={style.inputContainer}>
+                    <label htmlFor="tempo">
+                        Tempo
+                    </label>
+                    <input
+                        type="time"
+                        step="1"
+                        name="tempo"
+                        id="tempo"
+                        value={this.state.tempo}
+                        onChange={evento => this.setState({ ...this.state, tempo: evento.target.value })}
+                        min="00:00:00"
+                        max="01:30:00"
+                        required
+                    />
+                </div>
+                <Botao type="submit">
+                    Adicionar
+                </Botao>
+            </form>
+        )
+    }
+}
+
+export default Formulario;
